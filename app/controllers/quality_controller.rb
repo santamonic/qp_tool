7c64b93634bf3ef0b93ac07285cb5abf0fbd313f
santamonic <santamonic@naver.com>
2016-04-03 01:45:08 +0900

    add modify delete

--- app/controllers/quality_controller.rb
+++ app/controllers/quality_controller.rb
@@ -18,20 +18,60 @@ class QualityController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id]) 
   end
 
   def write
   end
 
   def write_complete
+    post = Post.new
+    post.quality_practice = params[:post_category]
+    post.toolname = params[:post_tool] 
+    post.title = params[:post_title] 
+    post.support_language = params[:post_language]
+    post.support_os = params[:post_os]
+    post.buy_need = params[:post_buyneed]
+    post.support_javaversion = params[:post_version]
+    post.urlname = params[:post_url] 
+    if post.save
+      flash[:alert] = "저장되었습니다."
+      redirect_to "/quality/show/#{post.id}"
+    else
+      flash[:alert] = post.errors.values.flatten.join(' ')
+      redirect_to :back
+    end
+
   end
 
   def edit
+    @post = Post.find(params[:id]) 
   end
 
   def edit_complete
+    post = Post.find(params[:id]) 
+    post.quality_practice = params[:post_category]
+    post.toolname = params[:post_tool] 
+    post.title = params[:post_title] 
+    post.support_language = params[:post_language]
+    post.support_os = params[:post_os]
+    post.buy_need = params[:post_buyneed]
+    post.support_javaversion = params[:post_version]
+    post.urlname = params[:post_url] 
+    if post.save
+      flash[:alert] = "수정되었습니다."
+      redirect_to "/quality/show/#{post.id}"
+    else
+      flash[:alert] = post.errors.values.flatten.join(' ')
+      redirect_to :back
+    end
+
   end
 
   def delete_complete
+    post = Post.find(params[:id])
+    post.destroy
+    flash[:alert] = "삭제되었습니다."
+    redirect_to "/"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'quality#posts'
 #  get "quality/posts_category" 
   get "/:quality_practice" => 'quality#posts_category'
-  get "quality/show"
+  get "quality/show/:id" => 'quality#show'
   get "quality/write"
-  get "quality/write_complete"
-  get "quality/edit"
-  get "quality/edit_complete"
-  get "quality/delete_complete"
+  post "quality/write_complete"
+  get "quality/edit/:id" => 'quality#edit'
+  post "quality/edit_complete"
+  get "quality/delete_complete/:id" => 'quality#delete_complete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
