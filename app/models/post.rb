8538ad3f89ba868d9506ab5f10c595a43e968809
santamonic <santamonic@naver.com>
2016-04-19 19:22:36 +0900

    perfect some design

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-    validates :quality_practice, :inclusion => { :in => ["Coding Guideline","Unit Test&Coverage","Static Analystics","Cyclomatic Complexity"], :message => "Coding Guideline, Unit Test&Coverage, Static Analystics, Cyclomatic Complexity 중 하나를 선택하셔야 합니다" }
+    validates :quality_practice, :inclusion => { :in => ["Coding Guideline","Unit Test&Coverage","Static Analystics","Cyclomatic Complexity"], :message => "Coding Guideline, Unit Test&Coverage, Static Analystics, Cyclomatic Complexity 중 하나를 선택하셔야 합니다. " }
 
     validates :toolname, :presence => { :message => "Tool Name 을 반드시 입력해주세요."}  
     validates :support_language, :presence => { :message => "지원하는 언어를 반드시 입력해주세요."} 
